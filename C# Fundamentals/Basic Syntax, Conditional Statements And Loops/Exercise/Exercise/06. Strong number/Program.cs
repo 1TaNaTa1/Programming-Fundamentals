7e960c6afa8449e69b7c5c6fdde3c7ce6aa199eb
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-10-29 18:31:48 +0200

    Just doing the exercises

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements And Loops/Exercise/Exercise/06. Strong number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sumOfDigits = 0;
+
+            int tempNum = num;
+            while (tempNum != 0)
+            {
+                int digitFactorial = 1;
+                int digit = tempNum % 10;
+
+                for (int i = digit; i > 0; i--)
+                {
+                    digitFactorial *= i;
+                }
+
+                sumOfDigits += digitFactorial;
+
+                tempNum /= 10;
+            }
+
+            if (num == sumOfDigits)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}
---
+++ C# Fundamentals/Basic Syntax, Conditional Statements And Loops/Exercise/Exercise/10. Rage Expenses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main()
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double priceOfHeadset = double.Parse(Console.ReadLine());
+            double priceOfMouse = double.Parse(Console.ReadLine());
+            double priceOfKeyboard = double.Parse(Console.ReadLine());
+            double priceOfDisplay = double.Parse(Console.ReadLine());
+
+            double expenses;
+
+            expenses = priceOfHeadset * (lostGames / 2);
+
+            expenses += priceOfMouse * (lostGames / 3);
+
+            expenses += priceOfKeyboard * (lostGames / 6);
+
+            expenses += priceOfDisplay * (lostGames / 12);
+
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+        }
+    }
+}
