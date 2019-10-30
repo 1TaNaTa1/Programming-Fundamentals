4afbe3b915e8dd37cc6b31be073026d7a0388fd0
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-10-30 13:29:56 +0200

    Add Lab

---
+++ 03. Arrays/Lab/01. Day of Week/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] daysAsText = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int dayAsNumber = int.Parse(Console.ReadLine());
+
+            if (dayAsNumber < 1 || dayAsNumber > 7)
+                Console.WriteLine("Invalid day!");
+            else
+                Console.WriteLine(daysAsText[dayAsNumber - 1]);
+        }
+    }
+}
---
+++ 03. Arrays/Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = Console.ReadLine().Split();
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                string firstElement = array[i];
+
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = firstElement;
+            }
+
+            foreach (string item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
