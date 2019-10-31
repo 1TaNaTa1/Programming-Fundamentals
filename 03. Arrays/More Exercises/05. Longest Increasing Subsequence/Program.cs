02fe81067aaa61ef99b9537455ca8f705ccb8a8f
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-10-31 21:44:11 +0200

    It isn't ready. Need to make it work!!!!

---
+++ 03. Arrays/More Exercises/05. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Longest_Increasing_Subsequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] len = new int[numbers.Length];
+
+            // Need to fix
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                len[i] = 1;
+
+                for (int j = 0; j < i - 1; j++)
+                {
+                    if(numbers[j] < numbers[i] && len[j] + 1 > len[i])
+                    {
+                        len[i] = 1 + len[j];
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', len));
+        }
+    }
+}
