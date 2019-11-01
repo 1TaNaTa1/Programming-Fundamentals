573b3c75afb14626800e29d0e1b4603d48b73adc
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-01 19:28:25 +0200

    Finished

---
+++ 08. Text Processing/Exercise/03. Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main()
+        {
+            var filePath = Console.ReadLine();
+
+            var indexOfSlash = filePath.LastIndexOf('\\') + 1;
+            var indexOfDot = filePath.LastIndexOf('.') + 1;
+
+            var fileName = filePath.Substring(indexOfSlash, indexOfDot - indexOfSlash - 1);
+            var fileExtension = filePath.Substring(indexOfDot, filePath.Length - indexOfDot);
+
+            Console.WriteLine("File name: {0}", fileName);
+            Console.WriteLine("File extension: {0}", fileExtension);
+        }
+    }
+}
