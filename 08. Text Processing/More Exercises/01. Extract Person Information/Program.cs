5fac70e1e1e3385bad3ff85a31a99b1b6822814c
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-01 19:29:47 +0200

    Done!

---
+++ 08. Text Processing/More Exercises/01. Extract Person Information/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                var text = Console.ReadLine();
+
+                var startIndex = text.IndexOf('@') + 1;
+                var length = text.IndexOf('|') - startIndex;
+
+                var name = text.Substring(startIndex, length);
+
+                startIndex = text.IndexOf('#') + 1;
+                length = text.IndexOf('*') - startIndex;
+
+                var age = int.Parse(text.Substring(startIndex, length));
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
---
+++ 08. Text Processing/More Exercises/05. HTML/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._HTML
+{
+    class Program
+    {
+        static void Main()
+        {
+            var title = Console.ReadLine();
+            PrintInHTMLFormat("h1", title);
+
+            var content = Console.ReadLine();
+            PrintInHTMLFormat("article", content);
+
+            var input = Console.ReadLine();
+
+            while(input != "end of comments")
+            {
+                PrintInHTMLFormat("div", input);
+                input = Console.ReadLine();
+            }
+        }
+
+        static void PrintInHTMLFormat(string tagName, string content)
+        {
+            Console.WriteLine($"<{tagName}>");
+            Console.WriteLine($"\t{content}");
+            Console.WriteLine($"</{tagName}>");
+        }
+    }
+}
