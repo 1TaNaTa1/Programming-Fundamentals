b4b1f219c54f8fb721d27c8d26c85108fd6bf045
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-01 19:28:13 +0200

    Finished

---
+++ 08. Text Processing/Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            var sum = 0;
+
+            var firstWord = input[0];
+            var secondWord = input[1];
+
+            var leftWord = secondWord;
+            var index = firstWord.Length;
+
+            if(firstWord.Length > secondWord.Length)
+            {
+                leftWord = firstWord;
+                index = secondWord.Length;
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                sum += firstWord[i] * secondWord[i];
+            }
+
+            for (int i = index; i < leftWord.Length; i++)
+            {
+                sum += leftWord[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
