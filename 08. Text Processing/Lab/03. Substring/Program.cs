359687122314e766819ff57c0087df114f807a56
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-01 19:38:06 +0200

    Done

---
+++ 08. Text Processing/Lab/03. Substring/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main()
+        {
+            var word = Console.ReadLine().ToLower();
+            var text = Console.ReadLine().ToLower();
+
+            while (true)
+            {
+                var index = text.IndexOf(word);
+
+                if (index < 0)
+                {
+                    break;
+                }
+                text = text.Remove(index, word.Length);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
