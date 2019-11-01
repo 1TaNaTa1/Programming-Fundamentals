faab6a9ddf929c8026b51c159c92011baa8bec99
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-01 19:38:18 +0200

    Done

---
+++ 08. Text Processing/Lab/04. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                string replacement = new string('*', bannedWords[i].Length);
+
+                text = text.Replace(bannedWords[i], replacement);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
