b2b743eeb03a5210565d77ed49d55445f8b9b88f
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-05 18:23:06 +0200

    Done

---
+++ 10. Regular Expressions/Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main()
+        {
+            var regex = new Regex(@"%(?<customer>[A-Z][a-z]+)%.*<(?<product>\w+)>.*\|(?<quantity>\d+)\|.*?(?<price>\d+.?\d{0,2})\$.*");
+
+            var input = Console.ReadLine();
+
+            var totalIncome = 0.0;
+
+            while (input != "end of shift")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    var customer = match.Groups["customer"].Value;
+                    var product = match.Groups["product"].Value;
+                    var quantity = int.Parse(match.Groups["quantity"].Value);
+                    var price = double.Parse(match.Groups["price"].Value);
+
+                    var totalPrice = quantity * price;
+
+                    Console.WriteLine($"{customer}: {product} - {totalPrice:f2}");
+
+                    totalIncome += totalPrice;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
