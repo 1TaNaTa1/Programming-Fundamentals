3fea63b8d28e5a7cf68d7fe4898a8f6d694cc1d1
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2019-11-05 18:22:52 +0200

    Done

---
+++ 10. Regular Expressions/Exercise/01. Furniture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main()
+        {
+            var regex = new Regex(@">>(?<name>[A-Za-z]+)<<(?<price>[0-9]+\.*[0-9]+)!(?<quantity>[0-9]+)");
+
+            var input = Console.ReadLine();
+
+            var totalPrice = 0.0;
+
+            var furnitureNames = new List<string>();
+
+            while (input != "Purchase")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    furnitureNames.Add(match.Groups["name"].Value);
+
+                    var currPrice = double.Parse(match.Groups["price"].Value);
+                    var currQuantity = int.Parse(match.Groups["quantity"].Value);
+
+                    totalPrice += currPrice * currQuantity;
+                }
+
+                input = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Bought furniture:");
+            if(furnitureNames.Count != 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, furnitureNames));
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
+        }
+    }
+}
